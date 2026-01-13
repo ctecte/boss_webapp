9bf0245ef21dc43f47db90e7b3758b25a69a6131
ctecte <legendarycte@gmail.com>
2026-01-13 17:47:27 +0800

    updated file path for convert_to_sql.py to point to raw_data/{excel} instead of just {excel}

--- convert_to_sql.py
+++ convert_to_sql.py
@@ -4,8 +4,6 @@ import pandas as pd
 conn = sql.connect('boss_data.db')
 cursor = conn.cursor()
 
-# what tables do i need? Or do i need nothing?
-
 file = '_merged_filtered.xlsx'
 excel_files = [
     f'2021-22_T2{file}',
@@ -14,19 +12,20 @@ excel_files = [
     f'2023-24_T1{file}',
     f'2023-24_T2{file}',
     f'2024-25_T1{file}',
-    f'2024-25_T2{file}'
+    f'2024-25_T2{file}',
+    f'2025-26_T1{file}'
 ]
 
 for excel in excel_files:
     print(excel)
-    df = pd.read_excel(excel)
+    df = pd.read_excel(f'raw_data/{excel}')
     print("read excel")
 
     df.columns = [col.strip().lower().replace(" ", "_") for col in df.columns]
     
     print("converting to sql")
     df.to_sql("boss", conn, if_exists="append", index=False)
-    print("done..")
+    print("done")
 
 conn.commit()
 conn.close()
